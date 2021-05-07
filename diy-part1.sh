409fb3bccfe7450d44b3fb1f912b05a1afd9745d
WikiHacker <naveed7us@gmail.com>
2021-05-07 20:18:57 +0800

    remove extra

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
 sed -i '$a src-git WikiHacker https://github.com/WikiHacker/openwrt-packages' feeds.conf.default
